4d275723b9f07c2afebab4216cb2d6e2a4760155
Aditya Sharma <99742149+aditya-sharma21@users.noreply.github.com>
2023-06-17 13:36:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+var input = document.getElementById("input");
+var output = document.getElementById("output");
+var inputType = document.getElementById("inputType");
+var outputType = document.getElementById("outputType");
+var inputTypeValue, outputTypeValue;
+
+input.addEventListener("keyup", myOutput);
+inputType.addEventListener("change", myOutput);
+outputType.addEventListener("change", myOutput);
+
+inputTypeValue = inputType.value;
+outputTypeValue = outputType.value;
+
+function myOutput() {
+  inputTypeValue = inputType.value;
+  outputTypeValue = outputType.value;
+
+  if (inputTypeValue === "centimeter" && outputTypeValue === "kilometer") {
+    output.value = Number(input.value) * 0.00001 + " Km";
+  } else if (inputTypeValue === "centimeter" && outputTypeValue === "meter") {
+    output.value = Number(input.value) * 0.01 + " m";
+  } else if (
+    inputTypeValue === "centimeter" &&
+    outputTypeValue === "centimeter"
+  ) {
+    output.value = input.value + " cm";
+  }
+
+  if (inputTypeValue === "meter" && outputTypeValue === "kilometer") {
+    output.value = Number(input.value) * 0.001 + " Km";
+  } else if (inputTypeValue === "meter" && outputTypeValue === "centimeter") {
+    output.value = Number(input.value) * 100 + " cm";
+  } else if (inputTypeValue === "meter" && outputTypeValue === "meter") {
+    output.value = input.value + " m";
+  }
+
+  if (inputTypeValue === "kilometer" && outputTypeValue === "meter") {
+    output.value = Number(input.value) * 1000 + " m";
+  } else if (
+    inputTypeValue === "kilometer" &&
+    outputTypeValue === "centimeter"
+  ) {
+    output.value = Number(input.value) * 100000 + " cm";
+  } else if (
+    inputTypeValue === "kilometer" &&
+    outputTypeValue === "kilometer"
+  ) {
+    output.value = input.value + " Km";
+  }
+}
